696f5b558cd4c530bac9f358a011e741265eef31
Matthieu Mota <matthieumota@gmail.com>
2020-12-14 15:10:30 +0100

    Une pizza dans Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pizza } from './pizza';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pizzaparty';
+  title = 'Pizza Party';
+  // maSuperPizza est "typée" avec la classe Pizza
+  maSuperPizza: Pizza = {
+    id: 1,
+    name: '4 fromages',
+    price: 12,
+    image: '4-fromages.jpg'
+  };
 }
---
+++ src/app/pizza.ts
@@ -0,0 +1,11 @@
+/**
+ * Cette classe me permet de structurer ce que
+ * contient une pizza. L'export permettra d'importer
+ * la classe partout dans mon application.
+ */
+export class Pizza {
+    id: number;
+    name: string;
+    price: number;
+    image: string;
+}
