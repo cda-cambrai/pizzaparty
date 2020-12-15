ca7352ccbf38e6902455d4b6ffb8c92c5b074760
Matthieu Mota <matthieumota@gmail.com>
2020-12-15 15:00:21 +0100

    Création des routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,25 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { PizzaListComponent } from './pizza-list/pizza-list.component';
 import { MenuComponent } from './menu/menu.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PizzaListComponent,
-    MenuComponent
+    MenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot([
+      // On définit toutes les routes (pages) du site
+      { path: '', component: HomeComponent },
+      { path: 'pizzas', component: PizzaListComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
