a025f6ecb18c312f018a37a59f65a9a3ff64ddbd
Matthieu Mota <matthieumota@gmail.com>
2020-12-15 17:09:45 +0100

    WIP promesse

--- src/app/services/pizza.service.ts
+++ src/app/services/pizza.service.ts
@@ -12,6 +12,13 @@ export class PizzaService {
    * Permet de récupérer les pizzas sur notre API...
    */
   getPizzas(): Pizza[] {
+
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(function (response) {
+        // Le code qui est ici va s'exécuter plus tard...
+        console.log(response);
+      });
+
     return [
       { id: 1, name: 'Reine', price: 12, image: 'reine.jpg' },
       { id: 2, name: '4 fromages', price: 13, image: '4-fromages.jpg' },
